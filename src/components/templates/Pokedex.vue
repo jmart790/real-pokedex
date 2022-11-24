5ea0266ec5cb17e82e7e30a0a3dec127e0b75bac
Joshua Martinez <joshualeemartinez@gmail.com>
2022-11-24 13:18:09 -0500

    added toast to component

--- src/components/templates/Pokedex.vue
+++ src/components/templates/Pokedex.vue
@@ -5,11 +5,17 @@
         <Radar />
       </template>
       <Window class="pokedex__left-window">
-        <!-- turns on, shows intro,  -->
+        <Toast
+          v-show="isToastVisible"
+          v-bind="toastProps.header[currentView]"
+        />
         <PokemonIntro v-if="currentView === 'INTRO'" />
         <PokemonList v-else-if="currentView === 'LIST'" />
         <PokemonDetails v-else-if="currentView === 'POKEMON'" />
-        <!-- shows pokemon individual -->
+        <Toast
+          v-show="isToastVisible"
+          v-bind="{ ...toastProps.footer[currentView] }"
+        />
       </Window>
       <LeftControls />
     </PokedexLeft>
@@ -22,17 +28,51 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { usePokeStore } from "@/store/pokemon";
-import { useControlsStore } from "@/store/controls";
-import { storeToRefs } from "pinia";
+import { onMounted } from 'vue';
+import { usePokeStore } from '@/store/pokemon';
+import { useControlsStore } from '@/store/controls';
+import { storeToRefs } from 'pinia';
+import { computed } from '@vue/reactivity';
 
 const pokeStore = usePokeStore();
 const controlsStore = useControlsStore();
 
-const { pokemonListLength } = storeToRefs(pokeStore);
+const { pokemonListLength, genNum, activePokemonName } = storeToRefs(pokeStore);
 const { currentView } = storeToRefs(controlsStore);
 const { setListLength, togglePower } = controlsStore;
+
+const isToastVisible = computed(() => {
+  // will need to compute future store value so user can hide toasts
+  const allowedViews = ['LIST', 'POKEMON'];
+  return allowedViews.includes(currentView.value);
+});
+
+const toastProps = computed(() => {
+  return {
+    header: {
+      LIST: {
+        isHeader: true,
+        copy: `Gen ${genNum.value} Pokemon`
+      },
+      POKEMON: {
+        isHeader: true,
+        copy: activePokemonName.value
+      }
+    },
+    footer: {
+      LIST: {
+        isHeader: false,
+        copy: `Select`,
+        btnCopy: 'A'
+      },
+      POKEMON: {
+        isHeader: false,
+        copy: 'Back',
+        btnCopy: 'B'
+      }
+    }
+  };
+});
 
 onMounted(async () => {
   await pokeStore.getGeneration();
