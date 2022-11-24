af182e9c50e8608faa90239d2c34fb847f8e3e67
Josh <jmart790@fiu.edu>
2022-11-23 19:35:37 -0500

    added command function for b

--- src/composables/useMainControls.ts
+++ src/composables/useMainControls.ts
@@ -3,19 +3,26 @@ import { useControlsStore } from "@/store/controls";
 export default function useMainControls() {
   const controlsStore = useControlsStore();
 
-  function handleMainControl(value: string) {
-    const command = value.toLocaleLowerCase().replace("arrow", "");
-
+  function handleMainControl(command: string) {
+    const { currentView, navigatePokemonList, togglePower, setMainView } =
+      controlsStore;
     switch (command) {
       case "power":
-      case "p":
-        controlsStore.togglePower();
+        togglePower();
         break;
       case "up":
       case "down":
       case "left":
       case "right":
-        controlsStore.navigatePokemonList(command);
+        navigatePokemonList(command);
+        break;
+      case "a":
+        // enter/go command
+        setMainView("POKEMON");
+        break;
+      case "b":
+        // back comman
+        setMainView("LIST");
         break;
       default:
         break;
@@ -23,6 +30,6 @@ export default function useMainControls() {
   }
 
   return {
-    handleMainControl
-  }
+    handleMainControl,
+  };
 }
