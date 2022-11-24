0c2c961d2ef7f73f6248bc5095abd6041cba20d8
Joshua Martinez <joshualeemartinez@gmail.com>
2022-11-24 13:17:47 -0500

    added temp bg image

--- src/components/views/PokemonDetails.vue
+++ src/components/views/PokemonDetails.vue
@@ -6,17 +6,17 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
-import { storeToRefs } from "pinia";
-import PokeAPI, { type IPokemon } from "pokeapi-typescript";
-import { usePokeStore } from "@/store/pokemon";
+import { computed, onMounted, ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import PokeAPI, { type IPokemon } from 'pokeapi-typescript';
+import { usePokeStore } from '@/store/pokemon';
 const pokeStore = usePokeStore();
 const { activePokemonName } = storeToRefs(pokeStore);
 
 const pokemon = ref<IPokemon>();
 const spriteImage = computed(
   () =>
-    pokemon?.value?.sprites?.versions["generation-v"]["black-white"].animated
+    pokemon?.value?.sprites?.versions['generation-v']['black-white'].animated
       .front_default
 );
 
@@ -31,9 +31,17 @@ onMounted(async () => {
 
 <style scoped lang="scss">
 .pokemon-details {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  background-repeat: round;
+  background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2fb2821a-1406-4a1d-9b04-6668f278e944/d83m36e-3b58ca48-fe8a-456e-9ffc-a5a84eca6613.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzJmYjI4MjFhLTE0MDYtNGExZC05YjA0LTY2NjhmMjc4ZTk0NFwvZDgzbTM2ZS0zYjU4Y2E0OC1mZThhLTQ1NmUtOWZmYy1hNWE4NGVjYTY2MTMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.jOpoPaOypPatcb4k7flznTP9YiwUEX2q2BKoeWUPU74');
   img {
-    height: 200px;
-    width: 200px;
+    position: absolute;
+    bottom: 20%;
+    right: 10%;
+    height: 150px;
+    width: 150px;
   }
 }
 </style>
