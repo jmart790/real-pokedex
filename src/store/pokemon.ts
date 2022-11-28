399c9c9f2468079fff78fe11403d9d39a78783b8
Joshua Martinez <joshualeemartinez@gmail.com>
2022-11-28 08:34:44 -0500

    added region and loaded state to pokemon list

--- src/store/pokemon.ts
+++ src/store/pokemon.ts
@@ -1,24 +1,34 @@
-import { defineStore } from "pinia";
-import { ref, computed } from "vue";
-import PokeAPI, { type IPokemon } from "pokeapi-typescript";
-import type { IGeneration } from "pokeapi-typescript";
+import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
+import PokeAPI, { type IPokemon } from 'pokeapi-typescript';
+import type { IGeneration } from 'pokeapi-typescript';
 
-export const usePokeStore = defineStore("pokemon", () => {
+interface IPokemonListItem {
+  name: string;
+  isLoaded: boolean;
+}
+export const usePokeStore = defineStore('pokemon', () => {
   const generation = ref<IGeneration>();
   const genNum = computed(() => generation.value?.id);
-  const pokemonList = computed(() => generation.value?.pokemon_species);
+  const region = computed(() => generation.value?.main_region.name);
+  const pokemonList = ref<IPokemonListItem[]>();
   const pokemonListLength = computed(
     () => generation.value?.pokemon_species.length
   );
 
   async function getGeneration(genNum = 1) {
     await PokeAPI.Generaition.resolve(genNum)
-      .then((res) => (generation.value = res))
+      .then((res) => {
+        generation.value = res;
+        pokemonList.value = generation.value?.pokemon_species.map(
+          ({ name }, index) => ({ name, isLoaded: index <= 20 })
+        );
+      })
       .catch((e) => console.log({ e }));
   }
 
   const activePokemon = ref<IPokemon>();
-  const activePokemonName = ref<string>("");
+  const activePokemonName = ref<string>('');
 
   function setActivePokemon(pokemon: IPokemon) {
     activePokemon.value = pokemon;
@@ -29,14 +39,21 @@ export const usePokeStore = defineStore("pokemon", () => {
     activePokemonName.value = name;
   }
 
+  function setPokemonLoaded(index: number) {
+    if (!pokemonList.value?.length) return;
+    pokemonList.value[index].isLoaded = true;
+  }
+
   return {
     getGeneration,
     pokemonList,
     pokemonListLength,
     genNum,
+    region,
     setActivePokemon,
     activePokemon,
     setActivePokemonName,
     activePokemonName,
+    setPokemonLoaded
   };
 });
