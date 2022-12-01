c43d76c3b08b077f9ae11c1421afd0cef7f227e0
Joshua Martinez <joshualeemartinez@gmail.com>
2022-12-01 13:56:14 -0500

    created icon component to consume icons

---
+++ src/components/atoms/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <component :is="icon" class="fill-current" />
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+const icon = defineAsyncComponent(
+  () => import(`../../assets/icons/${props.name}.svg`)
+);
+</script>
+
+<style lang="scss">
+.fill-current {
+  fill: inherit;
+}
+</style>
