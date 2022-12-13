49331b4f266dd8c7467ac45b469806b5c3288908
Joshua Martinez <joshualeemartinez@gmail.com>
2022-12-13 15:17:39 -0500

    added new option mode

--- src/components/atoms/TypePill.vue
+++ src/components/atoms/TypePill.vue
@@ -1,31 +1,51 @@
 <template>
-  <div class="type-pill" :class="`type-pill--${type}`">
-    <Icon class="type-pill__icon" :name="`type-${type}`" />
-    <span class="type-pill__copy">{{ type }}</span>
+  <div class="type-pill" :class="`type-pill--${type} type-pill--${mode}`">
+    <Icon
+      v-if="mode !== 'copy'"
+      class="type-pill__icon"
+      :name="`type-${type}`"
+    />
+    <span v-if="mode !== 'icon'" class="type-pill__copy">{{ type }} </span>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{ type: string }>();
+// correct type type
+type Mode = 'normal' | 'copy' | 'icon';
+withDefaults(defineProps<{ type: string; mode: Mode }>(), { mode: 'normal' });
 </script>
 
 <style scoped lang="scss">
 .type-pill {
+  $self: &;
+  width: fit-content;
   display: flex;
   align-items: center;
   background-color: $pokemon-grass;
   border-radius: 20px;
-  padding: gap(1) gap(3);
   &__icon {
     width: 25px;
     height: 25px;
     fill: rgba(white, 0.85);
+    filter: drop-shadow(0px 0px 4px rgba(black, 0.5));
   }
   &__copy {
     line-height: 1.2;
     text-transform: capitalize;
     color: rgba(white, 0.85);
     font-size: rem(14);
+    text-shadow: 0px 0px 4px rgba(black, 0.5);
+  }
+
+  &--normal {
+    padding: gap(1) gap(4) gap(1) gap(3);
+  }
+  &--icon {
+    border-radius: 50%;
+    #{$self}__icon {
+      width: 35px;
+      height: 35px;
+    }
   }
 
   &--fire {
