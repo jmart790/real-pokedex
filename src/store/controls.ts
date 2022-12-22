badd342a1a0cb684da6f3ddc17c47f55c77ac3bd
Josh <jmart790@fiu.edu>
2022-12-22 18:10:13 -0500

    created secondary view state and function

--- src/store/controls.ts
+++ src/store/controls.ts
@@ -1,17 +1,19 @@
-import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
 
-type CurrentView = "OFF" | "INTRO" | "LIST" | "POKEMON";
+type CurrentView = 'OFF' | 'INTRO' | 'LIST' | 'POKEMON';
+// type SecondaryView = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
-export const useControlsStore = defineStore("controls", () => {
-  const currentView = ref<CurrentView>("OFF");
-  const isPokedexOn = computed(() => currentView.value !== "OFF");
+export const useControlsStore = defineStore('controls', () => {
+  const currentView = ref<CurrentView>('OFF');
+  const secondaryView = ref<string>('1');
+  const isPokedexOn = computed(() => currentView.value !== 'OFF');
 
   function togglePower() {
-    if (currentView.value === "OFF") {
-      currentView.value = "INTRO";
-      setTimeout(() => (currentView.value = "LIST"), 6000);
-    } else currentView.value = "OFF";
+    if (currentView.value === 'OFF') {
+      currentView.value = 'INTRO';
+      setTimeout(() => (currentView.value = 'LIST'), 6000);
+    } else currentView.value = 'OFF';
   }
 
   const listPosition = ref(0);
@@ -23,32 +25,35 @@ export const useControlsStore = defineStore("controls", () => {
 
   function navigatePokemonList(command: string) {
     switch (command) {
-      case "up":
-        listPosition.value = getNextMove("SUB", 2);
+      case 'up':
+        listPosition.value = getNextMove('SUB', 2);
         break;
-      case "down":
-        listPosition.value = getNextMove("ADD", 2);
+      case 'down':
+        listPosition.value = getNextMove('ADD', 2);
         break;
-      case "left":
-        listPosition.value = getNextMove("SUB");
+      case 'left':
+        listPosition.value = getNextMove('SUB');
         break;
-      case "right":
-        listPosition.value = getNextMove("ADD");
+      case 'right':
+        listPosition.value = getNextMove('ADD');
         break;
       default:
         break;
     }
   }
 
-  function getNextMove(type: "ADD" | "SUB", num = 1) {
+  function getNextMove(type: 'ADD' | 'SUB', num = 1) {
     const position = listPosition.value;
     const end = listLength.value - 1;
-    if (type === "SUB") return position >= num ? position - num : 0;
+    if (type === 'SUB') return position >= num ? position - num : 0;
     else return position >= end - num ? end : position + num;
   }
 
   function setMainView(view: CurrentView) {
     currentView.value = view;
+  }
+  function setSecondaryView(view: string) {
+    secondaryView.value = view;
   }
 
   return {
@@ -59,5 +64,7 @@ export const useControlsStore = defineStore("controls", () => {
     currentView,
     setListLength,
     setMainView,
+    setSecondaryView,
+    secondaryView
   };
 });
