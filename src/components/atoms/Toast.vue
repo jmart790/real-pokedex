f46ef4606d044b858f23ebb08687e8e8a1208be4
Joshua Martinez <joshualeemartinez@gmail.com>
2022-12-24 09:39:23 -0500

    adjusted toast to not use pseudo elements

--- src/components/atoms/Toast.vue
+++ src/components/atoms/Toast.vue
@@ -1,12 +1,10 @@
 <template>
-  <component
-    :class="`toast--${isHeader ? 'title' : 'instructions'}`"
-    :is="isHeader ? 'h1' : 'p'"
-    class="toast"
-  >
-    <span v-if="btnCopy && !isHeader">{{ btnCopy }}</span>
-    {{ copy }}
-  </component>
+  <div class="toast" :class="`toast--${isHeader ? 'title' : 'instructions'}`">
+    <component class="toast__content" :is="isHeader ? 'h1' : 'p'">
+      <span v-if="btnCopy">{{ btnCopy }}</span>
+      {{ copy }}
+    </component>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -22,50 +20,34 @@ defineProps<Props>();
 .toast {
   position: absolute;
   z-index: 1;
-  padding: 4px 16px;
-  background: black;
-  font-size: 16px;
+  width: auto;
+  transform: skew(-60deg);
+  padding: 4px 32px;
   color: $off-white;
-  text-transform: capitalize;
+  background: black;
 
-  &::before {
-    position: absolute;
-    background-color: inherit;
-    z-index: -1;
-
-    content: ' ';
-    width: 65px;
-    height: 30px;
+  &__content {
+    font-size: 16px;
+    text-transform: capitalize;
+    transform: skew(60deg);
   }
 
   &--title {
     top: 0;
-    left: 0;
-    border-bottom-right-radius: 10px;
+    left: -28px;
+    padding-left: 44px;
+    border-bottom-right-radius: 5px;
     font-weight: 700;
-    &::before {
-      top: 0px;
-      right: -30px;
-      transform: skew(-40deg);
-      border-bottom-right-radius: 5px;
-    }
   }
   &--instructions {
-    height: fit-content;
+    bottom: 0;
+    right: -28px;
+    padding-right: 44px;
     display: flex;
     align-items: center;
     gap: 8px;
-    bottom: 0;
-    right: 0;
-    border-top-left-radius: 10px;
+    border-top-left-radius: 5px;
 
-    &::before {
-      height: 100%;
-      bottom: 0px;
-      left: -30px;
-      transform: skew(-40deg);
-      border-top-left-radius: 5px;
-    }
     span {
       display: inline-flex;
       justify-content: center;
