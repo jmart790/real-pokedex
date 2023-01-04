27ed185930981e9cc0b37de62d5faab7e3b6496c
Joshua Martinez <joshualeemartinez@gmail.com>
2023-01-03 21:11:42 -0500

    updated pill ui

--- src/components/atoms/TypePill.vue
+++ src/components/atoms/TypePill.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="type-pill" :class="`type-pill--${type} type-pill--${mode}`">
-    <Icon
-      v-if="mode !== 'copy'"
-      class="type-pill__icon"
-      :name="`type-${type}`"
-    />
-    <span v-if="mode !== 'icon'" class="type-pill__copy">{{ type }} </span>
-  </div>
+  <FrostCard>
+    <div class="type-pill" :class="`type-pill--${type} type-pill--${mode}`">
+      <Icon
+        v-if="mode !== 'copy'"
+        class="type-pill__icon"
+        :name="`type-${type}`"
+      />
+      <span v-if="mode !== 'icon'" class="type-pill__copy">{{ type }} </span>
+    </div>
+  </FrostCard>
 </template>
 
 <script setup lang="ts">
@@ -21,19 +23,18 @@ withDefaults(defineProps<{ type: string; mode: Mode }>(), { mode: 'default' });
   width: fit-content;
   display: flex;
   align-items: center;
-  background-color: $pokemon-grass;
-  border-radius: 20px;
-  box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2);
+  border-radius: inherit;
   &__icon {
     width: 25px;
     height: 25px;
     fill: rgba(white, 0.85);
-    filter: drop-shadow(0px 0px 4px rgba(black, 0.5));
+    filter: drop-shadow(1px 1px 4px rgba(black, 0.3));
   }
   &__copy {
     line-height: 1;
     text-transform: capitalize;
     color: rgba(white, 0.85);
+    text-shadow: 1px 1px 4px rgba(black, 0.3);
     font-size: rem(14);
   }
 
@@ -45,66 +46,72 @@ withDefaults(defineProps<{ type: string; mode: Mode }>(), { mode: 'default' });
   }
   &--icon {
     border-radius: 50%;
-    #{$self}__icon {
-      width: 35px;
-      height: 35px;
-    }
   }
 
-  @mixin radiant-bg($color-1, $color-2) {
-    background: $color-1;
-    background: radial-gradient(ellipse at center, $color-1 0%, $color-2 150%);
+  @mixin gradient-bg($color-1, $color-2) {
+    #{$self}__copy {
+      color: $color-2;
+    }
+    #{$self}__icon {
+      fill: $color-2;
+    }
+    // background: linear-gradient(40deg, $color-2 30%, $color-2 100%);
+    background: linear-gradient(
+      40deg,
+      rgba($color-1, 0.4) 30%,
+      rgba(255, 255, 255, 0.1) 100%
+    );
   }
   &--fire {
-    @include radiant-bg($pokemon-fire, $pokemon-fire-light);
+    @include gradient-bg($pokemon-fire, $pokemon-fire-light);
   }
   &--grass {
-    @include radiant-bg($pokemon-grass, $pokemon-grass-light);
+    @include gradient-bg($pokemon-grass, $pokemon-grass-light);
   }
   &--water {
-    @include radiant-bg($pokemon-water, $pokemon-water-light);
+    @include gradient-bg($pokemon-water, $pokemon-water-light);
   }
   &--normal {
-    @include radiant-bg($pokemon-normal, $pokemon-normal-light);
+    @include gradient-bg($pokemon-normal, $pokemon-normal-light);
   }
   &--poison {
-    @include radiant-bg($pokemon-poison, $pokemon-poison-light);
+    @include gradient-bg($pokemon-poison, $pokemon-poison-light);
   }
   &--bug {
-    @include radiant-bg($pokemon-bug, $pokemon-bug-light);
+    @include gradient-bg($pokemon-bug, $pokemon-bug-light);
   }
   &--ground {
-    @include radiant-bg($pokemon-ground, $pokemon-ground-light);
+    @include gradient-bg($pokemon-ground, $pokemon-ground-light);
   }
   &--fighting {
-    @include radiant-bg($pokemon-fighting, $pokemon-fighting-light);
+    @include gradient-bg($pokemon-fighting, $pokemon-fighting-light);
   }
   &--rock {
-    @include radiant-bg($pokemon-rock, $pokemon-rock-light);
+    @include gradient-bg($pokemon-rock, $pokemon-rock-light);
   }
   &--electric {
-    @include radiant-bg($pokemon-electric, $pokemon-electric-light);
+    @include gradient-bg($pokemon-electric, $pokemon-electric-light);
   }
   &--fairy {
-    @include radiant-bg($pokemon-fairy, $pokemon-fairy-light);
+    @include gradient-bg($pokemon-fairy, $pokemon-fairy-light);
   }
   &--psychic {
-    @include radiant-bg($pokemon-psychic, $pokemon-psychic-light);
+    @include gradient-bg($pokemon-psychic, $pokemon-psychic-light);
   }
   &--ghost {
-    @include radiant-bg($pokemon-ghost, $pokemon-ghost-light);
+    @include gradient-bg($pokemon-ghost, $pokemon-ghost-light);
   }
   &--ice {
-    @include radiant-bg($pokemon-ice, $pokemon-ice-light);
+    @include gradient-bg($pokemon-ice, $pokemon-ice-light);
   }
   &--dragon {
-    @include radiant-bg($pokemon-dragon, $pokemon-dragon-light);
+    @include gradient-bg($pokemon-dragon, $pokemon-dragon-light);
   }
   &--steel {
-    @include radiant-bg($pokemon-steel, $pokemon-steel-light);
+    @include gradient-bg($pokemon-steel, $pokemon-steel-light);
   }
   &--flying {
-    @include radiant-bg($pokemon-flying, $pokemon-flying-light);
+    @include gradient-bg($pokemon-flying, $pokemon-flying-light);
   }
 }
 </style>
