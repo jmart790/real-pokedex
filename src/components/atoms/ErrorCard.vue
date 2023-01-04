6943487034f7e3a7252b66724d85599a127ec6f8
Joshua Martinez <joshualeemartinez@gmail.com>
2023-01-04 18:55:13 -0500

    created basic error card

---
+++ src/components/atoms/ErrorCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <FrostCard>
+    <div class="error-card">
+      <p class="error-card__msg">
+        We're sorry, we are having some trouble fetching your data 🙁
+      </p>
+    </div>
+  </FrostCard>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.error-card {
+  @include flex-center;
+  height: 100%;
+  padding: $card-padding;
+  color: $off-white;
+  &__msg {
+    text-align: center;
+  }
+}
+</style>
