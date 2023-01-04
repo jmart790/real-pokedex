b4154b2c205c9d5518fee2a8add68d5abb250287
Joshua Martinez <joshualeemartinez@gmail.com>
2023-01-03 21:11:11 -0500

    updated frost card so items could overflow but not blur

--- src/components/atoms/FrostCard.vue
+++ src/components/atoms/FrostCard.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="frost-card">
-    <slot />
+    <div class="frost-card__content">
+      <slot />
+    </div>
+    <div class="frost-card__ui"></div>
   </div>
 </template>
 
@@ -8,14 +11,42 @@
 
 <style scoped lang="scss">
 .frost-card {
-  background: white;
-  background: linear-gradient(
-    40deg,
-    rgba(255, 255, 255, 0.3) 30%,
-    rgba(255, 255, 255, 0.1) 100%
-  );
-  backdrop-filter: blur(4px);
+  height: 100%;
+  position: relative;
+  z-index: 2;
   border-radius: $cool-border-radius;
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+
+  &__content {
+    height: inherit;
+    z-index: 2;
+    border-radius: inherit;
+  }
+  &__ui {
+    position: absolute;
+    inset: 0 0 0 0;
+    width: inherit;
+    height: inherit;
+    border-radius: inherit;
+    z-index: 1;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+    &:before {
+      content: '';
+      position: absolute;
+      inset: 0 0 0 0;
+      width: 100%;
+      height: 100%;
+      background: inherit;
+      z-index: 0;
+      border-radius: inherit;
+      background: white;
+      background: linear-gradient(
+        40deg,
+        rgba(255, 255, 255, 0.4) 30%,
+        rgba(255, 255, 255, 0.1) 100%
+      );
+      filter: blur(8px);
+    }
+  }
 }
 </style>
