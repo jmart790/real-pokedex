f10ee309b9d4e7545b3a3f430c6ff1a66e622d56
“Joshthefrontendguy” <josh@sioncentral.com>
2023-01-17 20:16:17 -0500

    created seperate actions per main view

--- src/composables/useMainControls.ts
+++ src/composables/useMainControls.ts
@@ -1,17 +1,23 @@
 import { useControlsStore } from '@/store/controls';
+import { usePokeStore } from '@/store/pokemon';
+import { storeToRefs } from 'pinia';
 
 export default function useMainControls() {
+  const pokeStore = usePokeStore();
   const controlsStore = useControlsStore();
 
-  const {
-    currentView,
-    navigatePokemonList,
-    togglePower,
-    setMainView,
-    setSecondaryView
-  } = controlsStore;
+  const { getGeneration } = pokeStore;
+  const { navigatePokemonList, togglePower, setMainView, setSecondaryView } =
+    controlsStore;
+
+  const { currentView } = storeToRefs(controlsStore);
 
   function handleMainControl(command: string) {
+    if (currentView.value === 'LIST') listViewControls(command);
+    else if (currentView.value === 'POKEMON') pokemonViewControls(command);
+  }
+
+  function listViewControls(command) {
     switch (command) {
       case 'power':
         togglePower();
@@ -27,7 +33,41 @@ export default function useMainControls() {
         setMainView('POKEMON');
         break;
       case 'b':
-        // back comman
+        // back command
+        // eventually menu cmd
+        break;
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+        getGeneration(Number(command));
+        break;
+      default:
+        break;
+    }
+  }
+
+  function pokemonViewControls(command) {
+    switch (command) {
+      case 'power':
+        togglePower();
+        break;
+      case 'up':
+      case 'down':
+      case 'left':
+      case 'right':
+        navigatePokemonList(command);
+        break;
+      case 'a':
+        // enter/go command
+        // nothing right now
+        break;
+      case 'b':
+        // back command
         setMainView('LIST');
         break;
       case '1':
