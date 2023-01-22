3f795da859f58611f13c60ad5f372c231a6f4112
“Joshthefrontendguy” <josh@sioncentral.com>
2023-01-22 09:03:59 -0500

    added sprite toggles

--- src/store/controls.ts
+++ src/store/controls.ts
@@ -1,13 +1,23 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
-
-type CurrentView = 'OFF' | 'INTRO' | 'LIST' | 'POKEMON';
-// type SecondaryView = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+import type { TMainView, TSecondaryView } from '@/types/index';
 
 export const useControlsStore = defineStore('controls', () => {
-  const currentView = ref<CurrentView>('OFF');
-  const secondaryView = ref<string>('1');
+  const currentView = ref<TMainView>('OFF');
+  const secondaryView = ref<TSecondaryView>(1);
   const isPokedexOn = computed(() => currentView.value !== 'OFF');
+  const isActiveSpriteFront = ref(true);
+  const activeSpriteType = ref(0);
+  const listPosition = ref(0);
+  const listLength = ref(0);
+
+  const spriteTypeOptions = ['animated', 'animatedShiny', 'artwork', 'artworkShiny'];
+  const activeSpriteSetting = computed(() => {
+    return {
+      type: spriteTypeOptions[activeSpriteType.value],
+      isFront: isActiveSpriteFront.value
+    };
+  });
 
   function togglePower() {
     if (currentView.value === 'OFF') {
@@ -16,8 +26,6 @@ export const useControlsStore = defineStore('controls', () => {
     } else currentView.value = 'OFF';
   }
 
-  const listPosition = ref(0);
-  const listLength = ref(0);
 
   function resetListPosition() {
     listPosition.value = 0;
@@ -54,11 +62,20 @@ export const useControlsStore = defineStore('controls', () => {
     else return position >= end - num ? end : position + num;
   }
 
-  function setMainView(view: CurrentView) {
+  function setMainView(view: TMainView) {
     currentView.value = view;
   }
-  function setSecondaryView(view: string) {
+  function setSecondaryView(view: TSecondaryView) {
     secondaryView.value = view;
+  }
+  function toggleActiveSpriteType() {
+    activeSpriteType.value =
+      activeSpriteType.value >= spriteTypeOptions.length - 1
+        ? 0
+        : activeSpriteType.value + 1;
+  }
+  function toggleActiveSpriteOrientation() {
+    isActiveSpriteFront.value = !isActiveSpriteFront.value;
   }
 
   return {
@@ -71,6 +88,9 @@ export const useControlsStore = defineStore('controls', () => {
     setListLength,
     setMainView,
     setSecondaryView,
-    secondaryView
+    secondaryView,
+    toggleActiveSpriteType,
+    toggleActiveSpriteOrientation,
+    activeSpriteSetting
   };
 });
