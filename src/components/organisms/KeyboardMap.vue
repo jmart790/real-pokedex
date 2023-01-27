7327a12433b05fd5b3e281718acc7bfb5c19e371
“Joshthefrontendguy” <josh@sioncentral.com>
2023-01-27 07:17:45 -0500

    added more corresponding keys

--- src/components/organisms/KeyboardMap.vue
+++ src/components/organisms/KeyboardMap.vue
@@ -2,7 +2,9 @@
   <section class="key-map" :class="{ 'key-map--open': isOpen }">
     <div class="key-map__header">
       <h4 class="key-map__title">Keyboard Controls</h4>
-      <button class="key-map__toggle" @click="isOpen = !isOpen">{{ isOpen ? 'close' : 'open' }}</button>
+      <button class="key-map__toggle" @click="isOpen = !isOpen">
+        {{ isOpen ? 'close' : 'open' }}
+      </button>
     </div>
     <div class="key-map__content">
       <div
@@ -25,7 +27,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const isOpen = ref(false);
-
 
 const buttons = [
   {
@@ -67,6 +68,51 @@ const buttons = [
     component: 'DpadButton',
     props: { variant: 'a' },
     correspondingKey: 'x'
+  },
+  {
+    component: 'BlueButton',
+    props: { copy: 1 },
+    correspondingKey: 1
+  },
+  {
+    component: 'BlueButton',
+    props: { copy: 2 },
+    correspondingKey: 2
+  },
+  {
+    component: 'BlueButton',
+    props: { copy: 3 },
+    correspondingKey: 3
+  },
+  {
+    component: 'BlueButton',
+    props: { copy: 4 },
+    correspondingKey: 4
+  },
+  {
+    component: 'BlueButton',
+    props: { copy: 5 },
+    correspondingKey: 5
+  },
+  {
+    component: 'BlueButton',
+    props: { copy: 6 },
+    correspondingKey: 6
+  },
+  {
+    component: 'BlueButton',
+    props: { copy: 7 },
+    correspondingKey: 7
+  },
+  {
+    component: 'BlueButton',
+    props: { copy: 8 },
+    correspondingKey: 8
+  },
+  {
+    component: 'PowerButton',
+    props: null,
+    correspondingKey: 'p'
   }
 ];
 </script>
@@ -75,13 +121,13 @@ const buttons = [
 .key-map {
   border: 6px solid $off-black;
   padding: gap(1) gap(2) gap(4) gap(2);
-  background: rgba($primary, .8);
+  background: rgba($primary, 0.8);
   border-radius: 2px;
   overflow: hidden;
   transition: all ease-in-out 300ms;
 
   height: 45px;
-  
+
   &--open {
     width: 100%;
     height: 100%;
@@ -105,7 +151,7 @@ const buttons = [
     border-radius: 5px;
     font-size: rem(12);
     cursor: pointer;
-    box-shadow: 2px 2px 8px rgba(black, .3);
+    box-shadow: 2px 2px 8px rgba(black, 0.3);
     &:hover {
       background-color: darken(white, 5%);
     }
@@ -123,7 +169,7 @@ const buttons = [
     display: flex;
     align-items: center;
     gap: gap(2);
-    background-color: rgba($pokemon-ground, .4);
+    background-color: rgba($pokemon-ground, 0.4);
     padding: gap(2);
     border-radius: 2px;
   }
@@ -131,8 +177,9 @@ const buttons = [
   &__button {
     width: 25px;
     height: 25px;
+    font-size: rem(16);
   }
-  
+
   &__key {
     @include flex-center;
     width: 40px;
@@ -140,13 +187,22 @@ const buttons = [
     padding: gap(1);
     background: $off-white;
     font-size: rem(12);
-    color: $dark-grey;
+    color: $medium-grey;
     border-radius: 0.3em;
-    border: 1px solid $dark-grey;
-    box-shadow: 0 0.2em 0 0.05em $medium-grey;
-    border-bottom-color: $light-grey;
     text-transform: capitalize;
-    cursor: default;
+    box-shadow: 0px 2px 1px 0px $lightest-grey inset,
+      0px -2px 1px 0px black inset, 0px 0px 2px 0px black, 0px 0px 2px 0px black,
+      0px 0px 2px 0px black;
+    user-select: none;
+    &:hover {
+      cursor: pointer;
+    }
+    &:active {
+      transform: translateY(1px);
+      box-shadow: 0px 1px 1px 0px $lightest-grey inset,
+        0px -1px 1px 0px black inset, 0px 0px 2px 0px black,
+        0px 0px 2px 0px black, 0px 0px 2px 0px black, 0px 0px 2px 0px black;
+    }
   }
 }
 </style>
