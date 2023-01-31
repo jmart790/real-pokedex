17dd7a4aed42fbb7b4776e502a24b4ba0c8d7b05
“Joshthefrontendguy” <josh@sioncentral.com>
2023-01-30 20:20:24 -0500

    created useLoading composable

---
+++ src/composables/useLoading.ts
@@ -0,0 +1,35 @@
+import { ref } from 'vue';
+
+type AsyncFunction = (...args: any[]) => Promise<any>;
+
+export function useLoading<T>(
+  asyncFunction: AsyncFunction,
+  minDuration = 1200
+) {
+  const isLoading = ref(true);
+
+  const executeFn = async (...args: any[]) => {
+    isLoading.value = true;
+    const start = Date.now();
+
+    try {
+      const result = await asyncFunction(...args);
+      const elapsed = Date.now() - start;
+
+      if (elapsed < minDuration) {
+        setTimeout(() => {
+          isLoading.value = false;
+        }, minDuration - elapsed);
+      } else {
+        isLoading.value = false;
+      }
+
+      return result;
+    } catch (error) {
+      isLoading.value = false;
+      throw error;
+    }
+  };
+
+  return { isLoading, executeFn };
+}
