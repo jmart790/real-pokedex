a2cb68551c058fd31fceb354b0238696cbf83df4
“Joshthefrontendguy” <josh@sioncentral.com>
2023-01-30 22:14:27 -0500

    updated box shadow for mobile btn

--- src/components/atoms/MobileViewButton.vue
+++ src/components/atoms/MobileViewButton.vue
@@ -46,7 +46,7 @@ function handleInternalClick() {
     color: $off-white;
     border-radius: $rounded-circle;
     border: 4px solid $off-black;
-    box-shadow: 4px 4px 8px rgba(black, 0.4);
+    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.4);
     &::before {
       position: absolute;
       z-index: -1;
