e49684453671860abc6eed73707d08c2f7379a7f
joshthefrontendguy <josh@sioncentral.com>
2023-03-09 15:33:47 -0500

    removed conditional for now

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,7 @@ import './assets/base.css';
 const pinia = createPinia();
 const app = createApp(App);
 
-const isProd = import.meta.env.PROD;
-if (!isProd) LogRocket.init('m9fwpv/real-pokedex');
+LogRocket.init('m9fwpv/real-pokedex');
 
 app.use(GlobalComponents);
 app.use(pinia);
