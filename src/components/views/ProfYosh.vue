1e61e3a43b8541de0b06a9c597208fad71a4e255
“Joshthefrontendguy” <josh@sioncentral.com>
2025-03-13 20:25:02 -0400

    feat: updated my bg img

--- src/components/views/ProfYosh.vue
+++ src/components/views/ProfYosh.vue
@@ -21,11 +21,7 @@
     content: '';
     background-position: left bottom;
     background-size: cover;
-    // background-image: url('https://cdn.discordapp.com/attachments/1057467541062168586/1080837755703144448/BonzaiYosh_landscape_of_a_jungle_with_a_waterfall_in_the_style__b3bcc660-211f-4694-860e-06c31980dec1.png');
-    // background-image: url('https://cdn.discordapp.com/attachments/1057467541062168586/1080838078010232832/BonzaiYosh_landscape_of_a_jungle_with_a_waterfall_in_the_style__c15c6045-dac9-43a0-946c-291d32e775f1.png');
-    // background-image: url('https://cdn.discordapp.com/attachments/1057467541062168586/1080838421444046929/BonzaiYosh_landscape_of_a_jungle_with_a_waterfall_in_the_style__b8fb07ed-2ae1-4a8c-862d-35b568d0b1db.png');
-    background-image: url('https://cdn.discordapp.com/attachments/1057467541062168586/1080838415878205520/BonzaiYosh_landscape_of_a_jungle_with_a_waterfall_in_the_style__77285c68-abc5-4c91-adf1-e23f3d231a2e.png');
-    // background-image: url('https://cdn.discordapp.com/attachments/1057467541062168586/1080838082972110969/BonzaiYosh_landscape_of_a_jungle_with_a_waterfall_in_the_style__2f3def86-a54a-418b-91e7-1e43b110c64e.png');
+    background-image: url('https://cdn.midjourney.com/77285c68-abc5-4c91-adf1-e23f3d231a2e/0_0.png');
     opacity: 0.9;
   }
 
